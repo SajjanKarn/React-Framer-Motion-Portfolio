13e123bdeefd1cf8507c8dd8ce663c24d54e59c1
SajjanKarn <karnaa787@gmail.com>
2024-02-10 10:41:05 +0545

    integrated google fonts and scss

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,16 @@
+import "./App.scss";
+
 export default function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <section>Hero</section>
+      <section>Parallax</section>
+      <section>Services</section>
+      <section>Parallax</section>
+      <section>Portfolio1</section>
+      <section>Portfolio2</section>
+      <section>Portfolio3</section>
+      <section>Contact</section>
+    </div>
+  );
 }
