84f84a91c8dcb661ef27a1d35fcbb8d38f9e8dcb
SajjanKarn <karnaa787@gmail.com>
2024-02-10 17:35:27 +0545

    added svg animations and links animation

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -4,28 +4,28 @@ import Links from "./links/Links";
 import ToggleButton from "./toggle-button/ToggleButton";
 import "./sidebar.scss";
 
+const variants = {
+  open: {
+    clipPath: "circle(1200px at 50px 50px)",
+    transition: {
+      type: "spring",
+      stiffness: 20,
+      // restDelta: 2,
+    },
+  },
+  closed: {
+    clipPath: "circle(30px at 50px 50px)",
+    transition: {
+      delay: 0.3,
+      type: "spring",
+      stiffness: 400,
+      damping: 40,
+    },
+  },
+};
+
 export default function Sidebar() {
   const [open, setOpen] = useState(false);
-
-  const variants = {
-    open: {
-      clipPath: "circle(1200px at 50px 50px)",
-      transition: {
-        type: "spring",
-        stiffness: 20,
-        // restDelta: 2,
-      },
-    },
-    closed: {
-      clipPath: "circle(30px at 50px 50px)",
-      transition: {
-        delay: 0.3,
-        type: "spring",
-        stiffness: 400,
-        damping: 40,
-      },
-    },
-  };
 
   return (
     <motion.div
--- src/components/sidebar/links/Links.jsx
+++ src/components/sidebar/links/Links.jsx
@@ -1,14 +1,45 @@
 import { motion } from "framer-motion";
 
+const variants = {
+  open: {
+    transition: {
+      staggerChildren: 0.1,
+    },
+  },
+  closed: {
+    transition: {
+      staggerChildren: 0.05,
+      staggerDirection: -1,
+    },
+  },
+};
+
+const itemVariants = {
+  open: {
+    y: 0,
+    opacity: 1,
+  },
+  closed: {
+    y: 50,
+    opacity: 0,
+  },
+};
+
 export default function Links() {
   const links = ["Home", "Services", "Portfolio", "Contact", "About"];
 
   return (
-    <motion.div className="links">
+    <motion.div className="links" variants={variants}>
       {links.map((link) => (
-        <a href={`#${link}`} key={link}>
+        <motion.a
+          href={`#${link}`}
+          key={link}
+          variants={itemVariants}
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.9 }}
+        >
           {link}
-        </a>
+        </motion.a>
       ))}
     </motion.div>
   );
